81dbd3625cd10c0a304abe1512b7fcd8680a10aa
NikhilDhaliya <nikhil11754@gmail.com>
2026-05-26 10:07:12 +0530

    fixed build

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import dotenv from "dotenv";
 import { setIoInstance } from "./socket/socket";
 import assignmentRoutes from "./assignment/assignment.route";
 
 import "./assignment/assignment.queue";
 import "./assignment/assignment.worker";
-
-dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
